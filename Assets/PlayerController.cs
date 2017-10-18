547a7013eef2d71664253655d92d6d78bc13d899
AKIHIRO NAGASHIMA <akihiro.nagashima@gmail.com>
2017-10-19 03:32:49 +0900

    Ïジャンプ機能の実装

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	public float jumpPower;
+	
+	private Rigidbody rd;
+
+    // Use this for initialization
+    void Start()
+    {
+		this.rd = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetButtonDown("Jump"))
+		{
+			this.rd.velocity = new Vector3(0, jumpPower, 0);
+		}
+    }
+}

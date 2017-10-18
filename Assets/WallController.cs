edfd6f8d68d6b162b16d230e002224c81677e26c
AKIHIRO NAGASHIMA <akihiro.nagashima@gmail.com>
2017-10-19 03:39:26 +0900

    障害物移動の実装

---
+++ Assets/WallController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallController : MonoBehaviour
+{
+	public float speed;
+
+	// Use this for initialization
+	void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		// speed * Time.deltaTime 一定時間に置ける進む距離を一定にする
+		transform.Translate(speed * Time.deltaTime, 0, 0);
+    }
+}

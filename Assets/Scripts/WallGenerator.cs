a8dae8b9f278ed011385a8232a5eb3e1e46ed588
AKIHIRO NAGASHIMA <akihiro.nagashima@gmail.com>
2017-10-19 16:40:11 +0900

    壁の自動生成の実装

---
+++ Assets/Scripts/WallGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallGenerator : MonoBehaviour
+{
+	public GameObject wallPrefab;
+	public float interval;
+
+	public float range = 3.0f;
+
+    // Use this for initialization
+    IEnumerator Start()
+    {
+		// コルーチン処理
+		while(true)
+		{
+			// 生成ポイントをランダムにする
+			transform.position = new Vector3(transform.position.x, Random.Range(-range, range), transform.position.z);
+
+			Instantiate(wallPrefab, transform.position, transform.rotation);
+			// intervalの時間、処理を中断
+			yield return new WaitForSeconds(interval);
+		}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
